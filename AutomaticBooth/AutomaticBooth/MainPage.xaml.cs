247bc27258af82c7ca65d558b9483c2dec93585f
Mike Taulty <mtaulty@microsoft.com>
2016-05-18 23:25:38 +0100

    Cheese working

--- AutomaticBooth/AutomaticBooth/MainPage.xaml.cs
+++ AutomaticBooth/AutomaticBooth/MainPage.xaml.cs
@@ -5,10 +5,12 @@
   using System.Threading.Tasks;
   using Windows.Foundation;
   using Windows.Graphics.Imaging;
+  using Windows.Media.SpeechRecognition;
   using Windows.UI.Xaml.Controls;
 
   public sealed partial class MainPage : Page, IPhotoControlHandler
   {
+    #region ALREADY_SEEN_THIS_CODE
     public MainPage()
     {
       this.InitializeComponent();
@@ -27,20 +29,6 @@
     public async Task OnClosingPhotoAsync(Guid photo)
     {
     }
-    public async Task OnModeChangedAsync(PhotoControlMode newMode)
-    {
-      switch (newMode)
-      {
-        case PhotoControlMode.Unauthorised:
-          break;
-        case PhotoControlMode.Grid:
-          break;
-        case PhotoControlMode.Capture:
-          break;
-        default:
-          break;
-      }
-    }
     public async Task<Rect?> ProcessCameraFrameAsync(SoftwareBitmap bitmap)
     {
       return (null);
@@ -50,5 +38,74 @@
       return (true);
     }
     Guid currentPhotoId;
+    #endregion // ALREADY_SEEN_THIS_CODE
+    public async Task OnModeChangedAsync(PhotoControlMode newMode)
+    {
+      switch (newMode)
+      {
+        case PhotoControlMode.Unauthorised:
+          break;
+        case PhotoControlMode.Grid:
+          break;
+        case PhotoControlMode.Capture:
+          await this.StartListeningForCheeseAsync();
+          break;
+        default:
+          break;
+      }
+    }
+
+    async Task StartListeningForCheeseAsync()
+    {
+      await this.StartListeningForConstraintAsync(
+        new SpeechRecognitionListConstraint(new string[] { "cheese" }));
+    }
+    async Task StartListeningForConstraintAsync(
+      ISpeechRecognitionConstraint constraint)
+    {
+      if (this.speechRecognizer == null)
+      {
+        this.speechRecognizer = new SpeechRecognizer();
+
+        this.speechRecognizer.ContinuousRecognitionSession.ResultGenerated 
+          += OnSpeechResult;
+      }
+      else
+      {
+        await this.speechRecognizer.ContinuousRecognitionSession.StopAsync();
+      }
+      this.speechRecognizer.Constraints.Clear();
+
+      this.speechRecognizer.Constraints.Add(constraint);
+
+      await this.speechRecognizer.CompileConstraintsAsync();
+
+      await this.speechRecognizer.ContinuousRecognitionSession.StartAsync();
+    }
+
+    async void OnSpeechResult(
+      SpeechContinuousRecognitionSession sender,
+     SpeechContinuousRecognitionResultGeneratedEventArgs args)
+    {
+      if ((args.Result.Confidence == SpeechRecognitionConfidence.High) ||
+          (args.Result.Confidence == SpeechRecognitionConfidence.Medium))
+      {
+        if (args.Result.Text.ToLower() == "cheese")
+        {
+          await this.Dispatcher.RunAsync(
+            Windows.UI.Core.CoreDispatcherPriority.Normal,
+            async () =>
+            {
+              var photoResult = await this.photoControl.TakePhotoAsync();
+
+              if (photoResult != null)
+              {
+              }
+            }
+          );
+        }
+      }
+    }
+    SpeechRecognizer speechRecognizer;   
   }
 }
