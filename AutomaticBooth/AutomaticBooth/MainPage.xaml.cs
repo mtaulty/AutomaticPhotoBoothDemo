84974f2f56195e646f829b306a876a44f81539ac
Mike Taulty <mtaulty@microsoft.com>
2016-05-18 23:22:03 +0100

    Added control with a null implementation

--- AutomaticBooth/AutomaticBooth/MainPage.xaml.cs
+++ AutomaticBooth/AutomaticBooth/MainPage.xaml.cs
@@ -1,30 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using Windows.UI.Xaml.Controls;
 
 namespace AutomaticBooth
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
+      this.InitializeComponent();
+      this.Loaded += OnLoaded;
     }
+    async void OnLoaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    {
+      // this won't work, it's just to get us to compile.
+      await this.photoControl.InitialiseAsync(null);
+    }
+  }
 }
