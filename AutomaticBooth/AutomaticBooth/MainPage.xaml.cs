8b662143393571544d22b83eabe1c7221d9a007f
Mike Taulty <mtaulty@microsoft.com>
2016-05-18 23:23:21 +0100

    Blank initialisation for control

--- AutomaticBooth/AutomaticBooth/MainPage.xaml.cs
+++ AutomaticBooth/AutomaticBooth/MainPage.xaml.cs
@@ -1,8 +1,13 @@
-﻿using Windows.UI.Xaml.Controls;
-
-namespace AutomaticBooth
+﻿namespace AutomaticBooth
 {
-  public sealed partial class MainPage : Page
+  using PhotoControlLibrary;
+  using System;
+  using System.Threading.Tasks;
+  using Windows.Foundation;
+  using Windows.Graphics.Imaging;
+  using Windows.UI.Xaml.Controls;
+
+  public sealed partial class MainPage : Page, IPhotoControlHandler
   {
     public MainPage()
     {
@@ -12,7 +17,38 @@ namespace AutomaticBooth
     async void OnLoaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
     {
       // this won't work, it's just to get us to compile.
-      await this.photoControl.InitialiseAsync(null);
+      await this.photoControl.InitialiseAsync(this);
     }
+
+    public async Task OnOpeningPhotoAsync(Guid photo)
+    {
+      this.currentPhotoId = photo;
+    }
+    public async Task OnClosingPhotoAsync(Guid photo)
+    {
+    }
+    public async Task OnModeChangedAsync(PhotoControlMode newMode)
+    {
+      switch (newMode)
+      {
+        case PhotoControlMode.Unauthorised:
+          break;
+        case PhotoControlMode.Grid:
+          break;
+        case PhotoControlMode.Capture:
+          break;
+        default:
+          break;
+      }
+    }
+    public async Task<Rect?> ProcessCameraFrameAsync(SoftwareBitmap bitmap)
+    {
+      return (null);
+    }
+    public async Task<bool> AuthoriseUseAsync()
+    {
+      return (true);
+    }
+    Guid currentPhotoId;
   }
 }
